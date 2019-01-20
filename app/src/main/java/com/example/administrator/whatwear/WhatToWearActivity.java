38b72ded640bd83282be9a621f0635dda453c940
fdrury <fdrury@ualberta.ca>
2019-01-20 12:11:04 -0700

    hooked up abother weather call

--- app/src/main/java/com/example/administrator/whatwear/WhatToWearActivity.java
+++ app/src/main/java/com/example/administrator/whatwear/WhatToWearActivity.java
@@ -2,12 +2,30 @@ package com.example.administrator.whatwear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WhatToWearActivity extends AppCompatActivity {
 
+    private float temperature = -1; // temperature is returned in Kelvin so -1 is a good error value
     private Button doneButton;
+    private EditText editText;
+    private String city = "Edmonton,CA";
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String API_KEY = "&appid=f28f345df9148259a4960bfbd2904024";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +40,58 @@ public class WhatToWearActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+                city = s.toString().replaceAll("\\s","");
+                String urlString = BASE_URL + "Edmonton,CA" + API_KEY;
+                try{
+                    JSONObject jsonObject = getJSONObjectFromURL(urlString);
+                    temperature = BigDecimal.valueOf(jsonObject.getDouble("temp")).floatValue();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    private static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        URL url = new URL(urlString);
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(10000 /* milliseconds */ );
+        urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+        urlConnection.setDoOutput(true);
+        urlConnection.connect();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        br.close();
+
+        String jsonString = sb.toString();
+        System.out.println("JSON: " + jsonString);
+
+        return new JSONObject(jsonString);
     }
 }
